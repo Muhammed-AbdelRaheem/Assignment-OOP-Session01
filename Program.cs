df234be218ac724d17bc60a7899e5920a98d6ce3
Muhammed-AbdelRaheem <MuhammedAbdelRaheem93@gmail.com>
2024-07-07 20:50:52 +0300

    Exception Handling and Protective Code

--- Program.cs
+++ Program.cs
@@ -2,9 +2,98 @@
 {
     internal class Program
     {
+
+        static void DoSomeCode()
+        {
+            try
+            {
+                int x, y, z;
+
+                x = int.Parse(Console.ReadLine()); // system.formatExp
+                y = int.Parse(Console.ReadLine());
+                z = x / y; //system.diviedbyzeroexp
+
+
+
+                int[] numbers = { 1, 2, 3 }; // siz 3
+                numbers[10] = 100; // system.indexoutofrange
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static void DoSomeProtictevCode()
+        {
+            int x, y, z;
+            bool flag;
+
+            do
+            {
+                Console.WriteLine("enter first Number");
+                flag = int.TryParse(Console.ReadLine(), out x);
+            }
+            while (!flag);
+
+            do
+            {
+                Console.WriteLine("enter Secound Number");
+                flag = int.TryParse(Console.ReadLine(), out y);
+            }
+            while (!flag || y == 0);
+
+            z = x / y;
+
+
+
+            int[] numbers = { 1, 2, 3 };
+            if (numbers?.Length > 10)
+            {
+                numbers[10] = 100;
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //DoSomeCode();
+
+            try
+            {
+                DoSomeProtictevCode();
+                throw   new Exception();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Close, Delete,Release
+                //Open File
+                //Conection With Database
+            }
+            Console.WriteLine(  "After Try Catch");
         }
     }
+
+
+
 }
+
+
+
